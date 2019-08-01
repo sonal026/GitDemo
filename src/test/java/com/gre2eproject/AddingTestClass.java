cc264bebb60710b9add06cf5c2e20788085880c5
sonal <sonal_026@yahoo.com>
2019-08-01 00:10:11 -0400

    Architect y first commit

--- src/test/java/com/gre2eproject/AddingTestClass.java
+++ src/test/java/com/gre2eproject/AddingTestClass.java
@@ -5,4 +5,8 @@ public class AddingTestClass {
 	public void testmethod(){
 		System.out.println("I am the new change");
 	}
+	
+	public void testdevelopmethod(){
+		System.out.println("I am the new change");
+	}
 }
